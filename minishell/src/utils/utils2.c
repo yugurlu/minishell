c361f12013555ce73e9349b5913fdbd8d6b33f80
yugurlu <yusufugurlu39@outlook.com>
2023-03-31 19:01:12 +0300

    s

--- minishell/src/utils/utils2.c
+++ minishell/src/utils/utils2.c
@@ -6,7 +6,7 @@
 /*   By: yugurlu <yugurlu@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:24:40 by yugurlu           #+#    #+#             */
-/*   Updated: 2023/03/28 11:18:15 by yugurlu          ###   ########.fr       */
+/*   Updated: 2023/03/31 19:00:30 by yugurlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,8 @@ int	isnumeric(char *str)
 			if (!ft_isdigit(str[i]))
 				return (0);
 		}
+		if((str[i] == '-' || str[i] == '+') && !str[i + 1])
+			return (0);
 		i++;
 	}
 	return (1);
